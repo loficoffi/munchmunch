1599f17e1bb2eb1b663c454a6e74ae7a756efb72
Raffael Friedl (loficoffi / 695a) <r.friedl@oth-aw.de>
2024-06-06 17:33:56 +0200

    - Added docker & nginx

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,21 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 import records from "./routes/record.js";
+import { fileURLToPath } from 'url';
 
 const PORT = process.env.PORT || 5050;
 const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.use(cors());
 app.use(express.json());
 app.use("/record", records);
+app.use(express.static(path.join(__dirname, "..", 'client', 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "..", 'client', 'dist', 'index.html'));
+});
 
 // start the Express server
 app.listen(PORT, () => {
