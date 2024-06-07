7a44be50afc9ad1b75af4f9dfbe2ef53ccb3de3e
Juliana Kuehn <juliana.kuehn@web.de>
2024-06-07 18:53:25 +0200

    changed navbar, is responsive now

--- src/client/src/components/Navbar.tsx
+++ src/client/src/components/Navbar.tsx
@@ -1,114 +1,155 @@
 import { NavLink } from "react-router-dom";
 import MunchLogo from '../assets/munchlogo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons'
 
-export default function Navbar() {
+import {
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+    Transition,
+} from '@headlessui/react'
+
+const navigation = [
+    { name: 'Home', href: '#', current: true },
+    { name: 'Meine Rezepte', href: '#', current: false },
+    { name: 'Kategorien', href: '#', current: false },
+    { name: <FontAwesomeIcon icon={ faSearch } />, href: '#', current: false },
+    { name: 'Zufällige Empfehlung', href: '#', current: false },
+]
+
+function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+}
+
+export default function NavigatonBar() {
     return (
-        <div>
-            <nav className="bg-black">
-                <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                    <div className="relative flex h-16 items-center justify-between">
-                        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                            <button type="button"
-                                    className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                                    aria-controls="mobile-menu" aria-expanded="false">
-                                <span className="absolute -inset-0.5"></span>
-                                <span className="sr-only">Open main menu</span>
-                                <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                     stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
-                                </svg>
-                                <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                     stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
-                                </svg>
-                            </button>
-                        </div>
-                        <div className="flex flex-1 mt-2 items-center justify-center sm:items-stretch sm:justify-start">
-                            <NavLink to="/">
-                                <img alt="MunchMunch Logo" className="h-12 w-auto" src={MunchLogo}></img>
-                            </NavLink>
-                            <div className="hidden sm:ml-6 sm:block">
-                                <div className="flex space-x-10">
-
-                                    <NavLink to="/"
-                                       className="text-white rounded-md px-3 py-2 text-sm font-medium"
-                                       aria-current="page">Home
-                                    </NavLink>
-                                    <NavLink to="/"
-                                       className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Meine Rezepte
-                                    </NavLink>
-                                    <NavLink to="/"
-                                       className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Kategorien
-                                    </NavLink>
-                                    <NavLink to="/"
-                                       className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                                        <FontAwesomeIcon icon={faSearch} />
-                                    </NavLink>
-                                    <NavLink to="/"
-                                             className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Zufällige Empfehlung
-                                    </NavLink>
+        <Disclosure as="nav" className="bg-black">
+            {({ open }) => (
+                <>
+                    <div className="mx-auto max-w-8xl px-2 sm:px-6 lg:px-8">
+                        <div className="relative flex h-16 items-center justify-between">
+                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                {/* Mobile menu button*/}
+                                <DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2 text-munch-orange hover:border border-munch-orange hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                    <span className="absolute -inset-0.5" />
+                                    <span className="sr-only">Open main menu</span>
+                                    {open ? (
+                                        <FontAwesomeIcon icon={ faBars } />
+                                    ) : (
+                                        <FontAwesomeIcon icon={ faBars } />
+                                    )}
+                                </DisclosureButton>
+                            </div>
+                            <div className="flex flex-1 mt-2 items-center justify-center sm:items-stretch sm:justify-start">
+                                <div className="flex flex-shrink-0 items-center">
+                                    <NavLink to="/">
+                                    <img alt="MunchMunch Logo" className="h-12 w-auto" src={MunchLogo}></img>
+                                </NavLink>
+                                </div>
+                                <div className="hidden sm:ml-6 sm:block">
+                                    <div className="flex space-x-4">
+                                        {navigation.map((item, index) => (
+                                            <a
+                                                key={index}
+                                                href={item.href}
+                                                className={classNames(
+                                                    item.current ? 'text-munch-orange underline underline-offset-8' : 'text-white hover:border border-munch-orange hover:text-white',
+                                                    'rounded-md px-3 py-2 text-sm font-regular'
+                                                )}
+                                                aria-current={item.current ? 'page' : undefined}
+                                            >
+                                                {item.name}
+                                            </a>
+                                        ))}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div
-                            className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
 
-                            <div className="relative ml-3">
-                                <div>
-                                    <button type="button"
-                                            className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                                            id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                        <span className="absolute -inset-1.5"></span>
-                                        <span className="sr-only">Open user menu</span>
-                                        <img className="h-8 w-8 rounded-full"
-                                             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                             alt=""/>
-                                    </button>
-                                </div>
-
-                                {/*<div
-                                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                                    role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
-                                    tabIndex="-1">
-                                    <!-- Active: "bg-gray-100", Not Active: "" -->
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem"
-                                       tabIndex="-1" id="user-menu-item-0">Your Profile</a>
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem"
-                                       tabIndex="-1" id="user-menu-item-1">Settings</a>
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem"
-                                       tabIndex="-1" id="user-menu-item-2">Sign out</a>
-                                </div>*/}
+                                {/* Profile dropdown */}
+                                <Menu as="div" className="relative ml-3">
+                                    <div>
+                                        <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                            <span className="absolute -inset-1.5" />
+                                            <span className="sr-only">Open user menu</span>
+                                            <img
+                                                className="h-8 w-8 rounded-full"
+                                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                                alt=""
+                                            />
+                                        </MenuButton>
+                                    </div>
+                                    <Transition
+                                        enter="transition ease-out duration-100"
+                                        enterFrom="transform opacity-0 scale-95"
+                                        enterTo="transform opacity-100 scale-100"
+                                        leave="transition ease-in duration-75"
+                                        leaveFrom="transform opacity-100 scale-100"
+                                        leaveTo="transform opacity-0 scale-95"
+                                    >
+                                        <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-black py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                            <MenuItem>
+                                                {({ focus }) => (
+                                                    <a
+                                                        href="#"
+                                                        className={classNames(focus ? 'text-munch-orange' : '', 'block px-4 py-2 text-sm text-white hover:text-munch-orange')}
+                                                    >
+                                                        Dein Profil
+                                                    </a>
+                                                )}
+                                            </MenuItem>
+                                            <MenuItem>
+                                                {({ focus }) => (
+                                                    <a
+                                                        href="#"
+                                                        className={classNames(focus ? 'text-munch-orange' : '', 'block px-4 py-2 text-sm text-white hover:text-munch-orange')}
+                                                    >
+                                                        Einstellungen
+                                                    </a>
+                                                )}
+                                            </MenuItem>
+                                            <MenuItem>
+                                                {({ focus }) => (
+                                                    <a
+                                                        href="#"
+                                                        className={classNames(focus ? 'text-munch-orange' : '', 'block px-4 py-2 text-sm text-white hover:text-munch-orange')}
+                                                    >
+                                                        Ausloggen
+                                                    </a>
+                                                )}
+                                            </MenuItem>
+                                        </MenuItems>
+                                    </Transition>
+                                </Menu>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <div className="sm:hidden" id="mobile-menu">
-                    <div className="space-y-1 px-2 pb-3 pt-2">
-
-                        <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
-                           aria-current="page">Dashboard</a>
-                        <a href="#"
-                           className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
-                        <a href="#"
-                           className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
-                        <a href="#"
-                           className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
-                    </div>
-                </div>
-            </nav>
-            {/*<nav className="flex justify-between items-center mb-6">
-                <NavLink to="/">
-                    <img alt="MunchMunch Logo" className="h-12 inline" src={MunchLogo}></img>
-                </NavLink>
-
-                <NavLink className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3" to="/create">
-                    Create Employee (Hallo Sandro!)
-                </NavLink>
-            </nav>*/}
-        </div>
-    );
+                    <DisclosurePanel className="sm:hidden">
+                        <div className="space-y-1 px-2 pb-3 pt-2">
+                            {navigation.map((item, index) => (
+                                <DisclosureButton
+                                    key={index}
+                                    as="a"
+                                    href={item.href}
+                                    className={classNames(
+                                        item.current ? 'text-munch-orange' : 'text-white hover:text-munch-orange',
+                                        'block rounded-md px-3 py-2 text-base font-regular'
+                                    )}
+                                    aria-current={item.current ? 'page' : undefined}
+                                >
+                                    {item.name}
+                                </DisclosureButton>
+                            ))}
+                        </div>
+                    </DisclosurePanel>
+                </>
+            )}
+        </Disclosure>
+    )
 }
