8bc062ed9855fcae93ff3a098000ad04531fd475
Raffael Friedl (loficoffi / 695a) <r.friedl@oth-aw.de>
2024-06-10 09:37:09 +0200

    - Updated AddButton.tsx

--- src/client/src/components/AddButton.tsx
+++ src/client/src/components/AddButton.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const AddButton = () => {
     return(
         <div>
-            <button type="submit" class="border-2 rounded-2xl">
+            <button type="submit" className="border-2 rounded-2xl">
                 <img src=".\src\assets\plusIcon.png" alt="Add" className="w-12 h-12"/>
             </button>
         </div>
