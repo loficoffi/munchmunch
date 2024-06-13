a3a37ec2ef6337fc8c89d44d7d7b47221674ba37
Juliana Kuehn <juliana.kuehn@web.de>
2024-06-13 17:02:16 +0200

    added props and structure for the tagcontainer

--- src/client/src/components/TagContainer.tsx
+++ src/client/src/components/TagContainer.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import Tag from "./Tag.tsx";
 
-const TagContainer = () => {
-    return(
-        <div>
-            <Tag/>
+interface TagContainerProps {
+    tags: {
+        name: string;
+        icon?: React.ReactNode;
+        backgroundColor: string;
+        onClick?: () => void;
+    }[];
+}
+
+const TagContainer: React.FC<TagContainerProps> = ({ tags }) => {
+    return (
+        //tags.map is for rendering more than one tags in tag-container
+        <div className="flex flex-wrap gap-2">
+            {tags.map((tag, index) => (
+                <Tag
+                    key={index}
+                    name={tag.name}
+                    icon={tag.icon}
+                    backgroundColor={tag.backgroundColor}
+                    onClick={tag.onClick}
+                />
+            ))}
         </div>
     );
 }
