69f335e983ddeecc5622acae25cfd75644cb09f9
Juliana Kuehn <juliana.kuehn@web.de>
2024-06-13 17:02:34 +0200

    added background colors for the foodcategory-tags

--- src/client/tailwind.config.js
+++ src/client/tailwind.config.js
@@ -8,8 +8,28 @@ export default {
     extend: {
       colors: {
         'munch-orange': '#FDA640',
+        'veggie-green': '#3F8345',
+        'vegan-yellow': '#FFF734',
+        'meat-rosa': '#B27777',
+        'fish-blue': '#6881DB',
       },
     },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
