572666836c239d462ff74b1fe267f202909056d1
Juliana Kuehn <juliana.kuehn@web.de>
2024-06-13 16:50:48 +0200

    added black background, changed w and h

--- src/client/src/components/AddButton.tsx
+++ src/client/src/components/AddButton.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const AddButton = () => {
     return(
         <div>
-            <button type="submit" className="border-2 rounded-2xl">
-                <img src=".\src\assets\plusIcon.png" alt="Add" className="w-12 h-12"/>
+            <button type="submit" className="border-2 rounded-2xl bg-black">
+                <img src=".\src\assets\plusIcon.png" alt="Add" className="w-10 h-10"/>
             </button>
         </div>
     )
