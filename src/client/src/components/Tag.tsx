27933f4004b2d2696a0e0aead8e070151ba154b7
Juliana Kuehn <juliana.kuehn@web.de>
2024-06-13 17:02:04 +0200

    added props and structure for the tag

--- src/client/src/components/Tag.tsx
+++ src/client/src/components/Tag.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 
-const Tag = () => {
-    return(
-        <div>im a tag</div>
+interface TagProps {
+    name: string;
+    icon?: React.ReactNode;
+    backgroundColor: string;
+    onClick?: () => void;
+}
+const Tag: React.FC<TagProps> = ({ name, icon, backgroundColor, onClick }) => {
+    return (
+        <div
+            className={`p-1 rounded-lg ${backgroundColor} text-sm text-center w-24 cursor-pointer`}
+            onClick={onClick}
+        >
+            <span className="mr-2">{icon}</span>
+            <span>{name}</span>
+        </div>
     )
 }
 
