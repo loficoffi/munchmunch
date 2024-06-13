a067714e93e686203e9848b2823cd20ee2bfa9b1
Juliana Kuehn <juliana.kuehn@web.de>
2024-06-13 16:51:46 +0200

    made it responsive, changed button-tag to link-tag for redirecting to another page, added AddButton

--- src/client/src/pages/Dashboard.tsx
+++ src/client/src/pages/Dashboard.tsx
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
+import {Link, redirect} from "react-router-dom";
+import AddButton from "../components/AddButton.tsx";
 import spagetthiBG from '../assets/images/spagetthi-test-bg.jpg';
 import chickenBG from '../assets/images/test-bg-dashboard.png'
 import '../index.css';
-import { Button } from '@headlessui/react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faUtensils} from '@fortawesome/free-solid-svg-icons'
 
@@ -24,22 +25,23 @@ export const Dashboard: React.FC = () => {
     }, []);
 
     const handleClick = () => {
+        redirect("/recipe");
         console.log('Button was clicked!');
     };
 
     return (
-        /*Hier noch Navbar evtl extra einfügen?*/
         <div>
             <div className="main-recipe-div">
-                <img src={backgroundImage} alt="Hintergrundbild"/>
-                <div className="main-recipe-infos">
-                    <h1 className="main-recipe-title">KNUSPRIGE ENTENBRUST SÜSS-SAUER</h1>
-                    <Button
-                        onClick={handleClick}
-                        className="rounded bg-black border border-munch-orange text-white py-2 px-4 text-sm text-white">
+                <img src={backgroundImage} className="bg-image" alt="Hintergrundbild"/>
+                <div className="main-recipe-infos w-2/4 md:w-1/2 mb-10 lg:w-2/3 xl:w-3/5">
+                    <h1 className="main-recipe-title text-2xl md:text-5xl lg:text-5xl xl:text-6xl">KNUSPRIGE ENTENBRUST SÜSS-SAUER</h1>
+                    <Link
+                        to="/recipe"
+                        className="btn btn-primary rounded bg-black border border-munch-orange text-white py-2 px-4 text-sm text-white">
                         <FontAwesomeIcon icon={ faUtensils } className="utensils-icon" />
                         <span className="to-recipe-title">Zum Gericht</span>
-                    </Button>
+                    </Link>
+                    <AddButton/>
                 </div>
             </div>
         </div>
