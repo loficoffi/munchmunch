8e4b1ec168f5bac90f9af6b141ce8abaf50d57ad
Raffael Friedl (loficoffi / 695a) <r.friedl@oth-aw.de>
2024-06-17 18:30:40 +0200

    - Migrated backend project to TypeScript - Added dev script - Adjusted .env files - Adjusted Dockerfile of backend project - Added nodemon & tsx for continous debug sessions in backend

---
+++ src/server/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+import './src/db/connection.js'; // Verbindung zur Datenbank
+import records from "./src/routes/record.js";
+import { fileURLToPath } from 'url';
+import authRoutes from './src/routes/auth.js'; // Neue Datei für Authentifizierungsrouten
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(cors());
+app.use(express.json());
+app.use("/record", records);
+app.use("/auth", authRoutes); // Authentifizierungsrouten hinzufügen
+app.use(express.static(path.join(__dirname, "..", 'client', 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "..", 'client', 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
---
+++ src/server/test/config/config.test.ts
@@ -0,0 +1,21 @@
+/*
+// mathOperations.test.js
+const mathOps = require('./mathOperations');
+
+describe('Math Operations Tests', () => {
+    // Test for addition
+    test('adds 1 + 2 to equal 3', () => {
+        // Arrange
+        const a = 1;
+        const b = 2;
+        const expected = 3;
+
+        // Act
+        const result = mathOps.add(a, b);
+
+        // Assert
+        expect(result).toBe(expected);
+
+        expect(result)
+    });
+});*/
