fcb4b25cf684ec71d3bd5a3a5262981f7d3bfa3b
Juliana Kuehn <juliana.kuehn@web.de>
2024-06-27 23:41:50 +0200

    added onClick function to favoriteButton

--- src/client/src/components/FavoriteButton.tsx
+++ src/client/src/components/FavoriteButton.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import heartIconImage from '../assets/heartRed.png';
 
-const FavoriteButton = () => {
+const FavoriteButton = ( { onClick }) => {
+
+    const handleClick = () => {
+        //go into onClick function which was given from component
+        if (onClick) {
+            onClick();
+        }
+    };
+
   return (
     <div>
       <button
         type="submit"
-        className="munch-mng-button fav-button border-2 rounded-2xl bg-black w-12 h-12 flex justify-center items-center">
+        className="munch-mng-button fav-button border-2 rounded-2xl bg-black w-12 h-12 flex justify-center items-center"
+        onClick={handleClick}>
         <img src={heartIconImage} alt="Add" className="w-8 h-8" />
       </button>
     </div>
