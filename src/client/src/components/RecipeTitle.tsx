a491b56b8c43a1164fdb16507eca9cb861387e9f
Juliana Kuehn <juliana.kuehn@web.de>
2024-06-30 12:44:00 +0200

    added some margin

--- src/client/src/components/RecipeTitle.tsx
+++ src/client/src/components/RecipeTitle.tsx
@@ -5,7 +5,7 @@ interface RecipeTitleProps {
 }
 
 const RecipeTitle: React.FC<RecipeTitleProps> = ({ recipeTitle }) => {
-  return <h2 className="text-white text-6xl">{recipeTitle}</h2>;
+  return <h2 className="text-white text-6xl mb-5">{recipeTitle}</h2>;
 };
 
 export default RecipeTitle;
